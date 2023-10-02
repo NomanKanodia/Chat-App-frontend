27db702b6d539fd2c0026bd9fe12c8c5f8b59445
NomanKanodia <nomankanodia@gmail.com>
2023-10-02 13:41:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  theme: {
+    
+    extend: {
+      colors:{
+        'primary': "#1476ff",
+        
+        'secondary':"#f3f5ff",
+        'Light':"#f9faff",
+  
+  
+      },
+    },
+  },
+  plugins: [],
+}
+
